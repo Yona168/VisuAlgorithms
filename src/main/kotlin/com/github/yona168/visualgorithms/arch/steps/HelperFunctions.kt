ab9a0e48bb344ee986661e98ddf011712d496b6b
Yonatan <yginsburg26@gmail.com>
2020-06-01 13:36:38 -0400

    Math Actions

--- src/main/kotlin/com/github/yona168/visualgorithms/arch/steps/HelperFunctions.kt
+++ src/main/kotlin/com/github/yona168/visualgorithms/arch/steps/HelperFunctions.kt
@@ -1,36 +1,64 @@
 package com.github.yona168.visualgorithms.arch.steps
 
-import com.github.yona168.visualgorithms.arch.algorithm
-
 class Var(val value: String)
 
-infix fun Var.with(int:Int)=Pair(this, int)
-infix fun Var.from(intPair:Pair<Int, Int>)=Pair(this, intPair)
+infix fun Var.with(int: Int) = Pair(this, int)
+infix fun Var.from(intPair: Pair<Int, Int>) = Pair(this, intPair)
 
 typealias VarIntPair = Pair<Var, Int>
+typealias VarDoublePair = Pair<Var, Double>
+typealias VarAnyPair = Pair<Var, Any>
 typealias VarStringPair = Pair<Var, String>
 
 class Result(internal val desc: String, internal val result: Any)
 
 fun ContainerStep.set(pair: Pair<Var, Any>) {
-    val desc=when(pair.second){
-        is Result->"Set ${pair.first.value} to ${(pair.second as Result).desc}"
-        else->"Set ${pair.first.value} to ${pair.second}"
+    val desc = when (pair.second) {
+        is Result -> "Set ${pair.first.value} to ${(pair.second as Result).desc}"
+        else -> "Set ${pair.first.value} to ${pair.second}"
     }
-    add(desc){
-        vars[pair.first.value]=(pair.second as Result).result
+    add(desc) {
+        vars[pair.first.value] = (pair.second as Result).result
     }
 }
 
-private fun ContainerStep.mathAction(pair: VarIntPair, desc: String, action: (Int, Int)->Int)=
-add("$desc ${pair.first.value} by ${pair.second}"){
-    val num=vars[pair.first.value] as Int
-    vars[pair.first.value]=action(num, pair.second)
+private fun <T : Number> ContainerStep.mathAction(pair: Pair<Var, T>, desc: String, action: (T, T) -> T) =
+    add("$desc ${pair.first.value} by ${pair.second}") {
+        val num = vars[pair.first.value] as T
+        vars[pair.first.value] = action(num, pair.second)
+    }
+
+fun ContainerStep.increment(pair: VarAnyPair) = when (pair.second) {
+    is Int -> mathAction(pair as VarIntPair, "Increment") { a, b -> a + b }
+    is Double -> mathAction(pair as VarDoublePair, "Increment") { a, b -> a + b }
+    else -> throw IllegalArgumentException("Cannot perform an increment operation on this kind of variable!")
 }
-fun ContainerStep.increment(pair: VarIntPair)=mathAction(pair, "Increment"){a,b -> a+b}
-fun ContainerStep.decrement(pair: VarIntPair)=mathAction(pair, "Decrement"){a,b->a-b}
-fun ContainerStep.multiply(pair:VarIntPair)=mathAction(pair, "Multiply"){a,b->a*b}
-fun ContainerStep.divide(pair:VarIntPair)=mathAction(pair, "Divide"){a,b->a/b}
+
+fun ContainerStep.decrement(pair: VarAnyPair) = when (pair.second) {
+    is Int -> mathAction(pair as VarIntPair, "Decrement") { a, b -> a - b }
+    is Double -> mathAction(pair as VarDoublePair, "Decrement") { a, b -> a - b }
+    else -> throw IllegalArgumentException("Cannot perform a decrement operation on this kind of variable!")
+}
+
+fun ContainerStep.multiply(pair: VarAnyPair) = when (pair.second) {
+    is Int -> mathAction(pair as VarIntPair, "Multiply") { a, b -> a * b }
+    is Double -> mathAction(pair as VarDoublePair, "Multiply") { a, b -> a * b }
+    else -> throw IllegalArgumentException("Cannot perform a divide operation on this kind of variable!")
+}
+
+fun ContainerStep.divide(pair: VarAnyPair) = when (pair.second) {
+    is Int -> mathAction(pair as VarIntPair, "Divide") { a, b -> a / b }
+    is Double -> mathAction(pair as VarDoublePair, "Divide") { a, b -> a / b }
+    else -> throw IllegalArgumentException("Cannot perform a division operation on this kind of variable!")
+}
+
+fun ContainerStep.modulo(pair: VarAnyPair) = when (pair.second) {
+    is Int -> mathAction(pair as VarIntPair, "Modulo") { a, b -> a % b }
+    is Double -> mathAction(pair as VarDoublePair, "Modulo") { a, b -> a % b }
+    else -> throw IllegalArgumentException("Cannot perform a modulo operation on this kind of variable!")
+}
+
+
 /*
 fun ContainerStep.substring(pair: Pair<Any, Pair<Int, Int>>):Result{
     val first=pair.first
